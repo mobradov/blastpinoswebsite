47892154b1a0e839eb2585683776e206ff3c4387
calicadev <66971665+calicadev@users.noreply.github.com>
2023-07-31 20:46:58 +0200

    Update ThirdSection.js

--- src/components/Content/ThirdSection.js
+++ src/components/Content/ThirdSection.js
@@ -5,14 +5,9 @@ const ThirdSection = () => {
     return (
         <div className="relative flex flex-col items-center justify-center text-center my-24">
             <div className="flex flex-col items-center justify-center w-7/12 mb-12">
-                <h1 className="custom-heading my-8">Ether City</h1>
+                <h1 className="custom-heading my-8">Based City</h1>
                 <p className="custom-paragraph text-start px-6">
-                    Introducing the famed Ether City, a place of opportunity,
-                    friendship and love. A bustling metropolis that never ceases
-                    to amaze its inhabitants, attracting tourists from other
-                    chains and entrepreneurs from around the world. All Ethereum
-                    NFTs are from here, and Ether Fellas are the most sought
-                    after.
+                    Introducing Based City, a place filled with on-chain degeneracy, gambling and some random bald guy. All Base Layer NFTs and tokens are welcome here, no SEC or feds!
                 </p>
             </div>
             <Image
